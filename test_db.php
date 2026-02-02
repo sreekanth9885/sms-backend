9513afc9d0366242354e9b3c1d4398681ccd4818
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-02-02 17:33:08 +0530

    testing ci cd

--- test_db.php
+++ test_db.php
@@ -1,12 +1,3 @@
 <?php
-
-try {
-    $pdo = new PDO(
-        "mysql:host=localhost;dbname=testdb;charset=utf8mb4",
-        "root",
-        "password" // put password if exists
-    );
-    echo "DB CONNECTED";
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
+echo "<h1>API is working fine and connected to the database.</h1>";
+?>
