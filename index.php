06b90e72e196f19a51ab9ab7f40dea83d8166e4f
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-02-03 13:02:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
+require_once __DIR__ . '/config/cors.php';
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/app/Core/Response.php';
 require_once __DIR__ . '/app/Core/Router.php';
 require_once __DIR__ . '/app/Controllers/AuthController.php';
 require_once __DIR__ . '/app/Controllers/UserController.php';
 require_once __DIR__ . '/app/Controllers/StatusController.php';
-require_once __DIR__ . '/config/cors.php';
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET,POST, OPTIONS");
 
 $router = new Router();
 
