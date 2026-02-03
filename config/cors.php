d800ad6f6afad2400526c3c3536ef4dd02036b5a
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-02-03 13:44:29 +0530

    cors issue

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,6 @@
 $allowedOrigins = [
     "http://localhost:5173",
     "https://sms.academicprojects.org",
-    "http://localhost:8000",
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
@@ -13,12 +12,6 @@ if ($origin && in_array($origin, $allowedOrigins)) {
     header("Vary: Origin");
 }
 
-header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-// Handle preflight
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+header("Access-Control-Allow-Credentials: true");
