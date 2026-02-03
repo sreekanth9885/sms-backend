19607ce3cd053db21677a2a70c558a1f2b82d875
sreekanth9885 <srikanth.bethi01@gmail.com>
2026-02-03 12:25:03 +0530

    added cors issue

---
+++ config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+$allowedOrigins = [
+    "http://localhost:5173",
+    "https://sms.academicprojects.org"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/app/Core/Router.php';
 require_once __DIR__ . '/app/Controllers/AuthController.php';
 require_once __DIR__ . '/app/Controllers/UserController.php';
 require_once __DIR__ . '/app/Controllers/StatusController.php';
+require_once __DIR__ . '/config/cors.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET,POST, OPTIONS");
